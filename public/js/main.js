64a828cf6578f5e54b6f8d57a647bda78c3e0752
Serezha Lev <>
2023-02-08 17:24:13 +0600

    async messaging added

--- public/js/main.js
+++ public/js/main.js
@@ -10,7 +10,15 @@ $(document)
             success: function(data) {
                 let text = input.val();
                 input.val('');
+                let prototype = $('.message-from-me-prototype').clone();
+                prototype.removeClass('message-from-me-prototype');
+                prototype.find('p.message-text').text(text);
+
+                let date = new Date();
+	            let time = date.getHours()+":"+date.getMinutes();
                 
+                prototype.find('p.message-time').text(time)
+                $('.chat-window').append(prototype);
             }
         });
     })
