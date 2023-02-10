6c448fe21b6980b165611dd717544a46dfd740de
Serezha Lev <>
2023-02-10 15:42:09 +0600

    User added

---
+++ src/Controller/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    public function register()
+    {
+        
+    }
+}
